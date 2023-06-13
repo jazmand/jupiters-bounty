6bb4134106eee2caaf9d5e5af279f5648a11f854
Ryan <ryan.a.y.johnson@gmail.com>
2023-06-14 01:45:13 +0900

    Update camera motion to use delta value & add camera boundaries

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,10 +1,12 @@
 extends Camera2D
 
-var zoomSpeed: float = 0.05
-var zoomMin: float = 0.01
-var zoomMax: float = 2.0
 var input_direction: Vector2 = Vector2.ZERO
 var camera_motion: Vector2 = Vector2.ZERO
+var camera_speed: float = 500.0
+
+# TODO: Add zoom function
+var zoomMin: float = 0.01
+var zoomMax: float = 2.0
 
 
 func _input(event: InputEvent) -> void:
@@ -23,13 +25,19 @@ func _input(event: InputEvent) -> void:
 			input_direction = Vector2.ZERO
 
 func _process(delta):
-	var fps = Engine.get_frames_per_second()
+	
+	# Reconsider using fps instead of delta
+	# var fps = Engine.get_frames_per_second()
+	
 	# Move the camera based on the input direction
 	if input_direction == Vector2.ZERO:
 		camera_motion = camera_motion.lerp(Vector2.ZERO, 0.2)
 	else:
-		camera_motion = input_direction * (fps / 4)
+		camera_motion = input_direction * (delta * camera_speed)
 		
-	print(delta)
-	print(camera_motion)
 	position += camera_motion
+
+	# Clamp the camera's position to the desired boundaries
+	# TODO: Update boundaries
+	position.x = clamp(position.x, 0, 1000)
+	position.y = clamp(position.y, 0, 500)
