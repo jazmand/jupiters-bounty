e258a466ba871a9894f9ccf0664b6addcbf84e74
Ryan <ryan.a.y.johnson@gmail.com>
2023-10-26 00:31:39 +0900

    Add room type data to build buttons & remove unused room_types

--- scripts/ui/Build.gd
+++ scripts/ui/Build.gd
@@ -4,14 +4,15 @@ extends Control
 
 var build_mode = false
 var room_selected = false
+var selected_room_type_id: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var main_node = get_tree().root.get_node("Main")
-	for room in main_node.room_types:
+	for room_type in main_node.room_types:
 		var button = Button.new()
-		button.text = room.name
-		button.pressed.connect(_on_room_selected)
+		button.text = room_type.name
+		button.pressed.connect(_on_room_selected.bind(room_type)) # Must "bind" to pass param to a connect callback.
 		button.mouse_entered.connect(_on_room_panel_mouse_entered)
 		button.mouse_exited.connect(_on_room_panel_mouse_exited)
 		$RoomPanel/HBoxContainer.add_child(button)
@@ -25,8 +26,10 @@ func _on_build_close_button_pressed():
 	$RoomPanel.visible = false
 	$BuildButton.visible = true
 	
-func _on_room_selected():
+func _on_room_selected(room_type):
 	room_selected = true
+	print(room_type.price)
+	selected_room_type_id = room_type.id
 	
 # Need way to clear initial tile when build_mode == false
 func _on_room_panel_mouse_entered():
