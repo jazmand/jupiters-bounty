bdf7c1dfca9fa27a9663f39a711286b4f1fe76e2
Ryan <ryan.a.y.johnson@gmail.com>
2024-02-07 23:30:20 +0900

    Make sprite face direction of movement

--- scripts/crew/CrewMember.gd
+++ scripts/crew/CrewMember.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 @onready var state_manager: StateChart = $CrewStateManager
 @onready var navigation_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 @onready var navigation_timer: Timer = $Navigation/Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	Global.station.crew += 1
@@ -23,12 +24,13 @@ func actor_setup():
 
 func set_movement_target(movement_target: Vector2) -> void:
 	navigation_agent.target_position = movement_target
+	
 
 func randomise_target_position() -> void:
-	target.position = Vector2(randf_range(0.0, 4000.0), randf_range(0, 2000.0))
+	target.position = Vector2(randf_range(2500.0, 6500.0), randf_range(1500.0, 3000.0))
 	set_movement_target(target.position)
 	while !navigation_agent.is_target_reachable():
-		target.position = Vector2(randf_range(0.0, 4000.0), randf_range(0, 2000.0))
+		target.position = Vector2(randf_range(2500.0, 6500.0), randf_range(1500.0, 3000.0))
 		set_movement_target(target.position)
 
 func _on_walking_state_physics_processing(delta: float) -> void:
@@ -37,10 +39,36 @@ func _on_walking_state_physics_processing(delta: float) -> void:
 		randomise_target_position()
 		print("new target: ", target.position)
 		return
-
+		
 	var direction: Vector2 = (navigation_agent.get_next_path_position() - global_position).normalized()
 	velocity = velocity.lerp(direction * speed, acceleration * delta)
+	
+	var angle = rad_to_deg(direction.angle())
+	print(angle)
 
+	if angle >= 22.5 and angle < 67.5:
+		animation_player.play('idle_down_right')
+	elif angle >= 67.5 and angle < 112.5:
+		animation_player.play('idle_down')
+	elif angle >= 112.5 and angle < 157.5:
+		animation_player.play('idle_down_left')
+	elif angle >=  157.5 and angle < 180 or angle >= -180 and angle < -147.5:
+		animation_player.play('idle_left')
+	elif angle <= -12.5 and angle > -57.5:
+		animation_player.play('idle_up_right')
+	elif angle <= -57.5 and angle > -102.5:
+		animation_player.play('idle_up')
+	elif angle <= -102.5 and angle > -147.5:
+		animation_player.play('idle_up_left')
+	else:
+		animation_player.play('idle_right')
+		
+#	if rad_to_deg(direction.angle() < 0):
+#		animation_player.play('idle_down_right')
+#	else:
+#		animation_player.play('idle_up_right')
+
+	
 	move_and_slide()
 
 
