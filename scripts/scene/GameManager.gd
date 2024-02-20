5c5397fd9dca158d29bb398b5171fb75781aa417
Ryan <ryan.a.y.johnson@gmail.com>
2024-02-20 22:43:10 +0900

    Add "Add Crew" button to add multiple crew

--- scripts/scene/GameManager.gd
+++ scripts/scene/GameManager.gd
@@ -5,9 +5,14 @@ extends Node
 
 const crew_scene: PackedScene = preload("res://crew.tscn")
 
+func _ready():
+	var gui_manager = get_node("/root/GUI/GUIManager")
+	gui_manager.connect("add_crew_pressed", Callable(self, "new_crew_member"))
+
 func new_crew_member() -> CrewMember:
 	var crew_member: CrewMember = crew_scene.instantiate()
-	crew_member.position = Vector2(1500,  2500) # Adjust spawning position
+	crew_member.position = Vector2(3000,  2000) # Adjust spawning position
 	add_child(crew_member)
 	return crew_member
+	
 	
