34bdf3bb3151dcd73e4bcba1d7ba499a8949e843
Conall <conallimlah@gmail.com>
2024-02-22 22:06:31 +0900

    hopefully this helps

--- scripts/crew/CrewMember.gd
+++ scripts/crew/CrewMember.gd
@@ -29,13 +29,10 @@ var current_animation = ''
 func _ready() -> void:
 	Global.station.crew += 1
 	navigation_timer.timeout.connect(_on_timer_timeout)
-	navigation_agent.path_desired_distance = 4.0
-	navigation_agent.target_desired_distance = 4.0
 	call_deferred("actor_setup")
 
 func actor_setup():
 	await get_tree().physics_frame
-	print("target: ", target)
 	set_movement_target(target)
 
 func set_movement_target(movement_target: Vector2) -> void:
@@ -78,7 +75,8 @@ func _on_walking_state_physics_processing(delta: float) -> void:
 	if navigation_agent.is_navigation_finished():
 		randomise_target_position()
 		return
-		
+	
+	set_rounded_direction()	
 	velocity = velocity.lerp(current_direction.normalized() * speed, acceleration * delta)
 	#var collision = move_and_collide(velocity * delta)
 	#if collision:
@@ -89,7 +87,5 @@ func _on_walking_state_physics_processing(delta: float) -> void:
 func _on_timer_timeout():
 	if !navigation_agent.is_target_reachable() or (abs(target.x - global_position.x) < 200 and abs(target.y - global_position.y) < 200):
 		randomise_target_position()
-	set_movement_target(target)
-	set_rounded_direction()
 	set_current_animation()
 	animation_player.play(current_animation)
