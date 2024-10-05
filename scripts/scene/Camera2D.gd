50d0045663f4a29efe610fbbe725c003058bfd80
jazmand <atothezmand@gmail.com>
2024-10-06 00:27:30 +0900

    Update input_direction directly from get_action_strength() and move to _process() function

--- scripts/scene/Camera2D.gd
+++ scripts/scene/Camera2D.gd
@@ -9,20 +9,6 @@ var zoom_min: float = 0.2
 var zoom_max: float = 1
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		var key_event: InputEventKey = event
-
-		# Check if an arrow key was pressed
-		if key_event.pressed:
-			# Calculate the input direction
-			input_direction = Vector2(
-				Input.get_action_strength("right") - Input.get_action_strength("left"),
-				Input.get_action_strength("down") - Input.get_action_strength("up")
-			)
-			# Reset the input direction when the arrow key is released
-		elif key_event.pressed == false:
-			input_direction = Vector2.ZERO
-			
 	# Check for mouse scroll inputs
 	if event is InputEventMouseButton:
 		var motion_event: InputEventMouseButton = event
@@ -33,7 +19,7 @@ func _input(event: InputEvent) -> void:
 			update_zoom(zoom_delta)
 		elif motion_event.button_index == 5:
 			update_zoom(-zoom_delta)
-			
+
 	# Check for pan gestures
 	if event is InputEventPanGesture:
 		var pan_event: InputEventPanGesture = event
@@ -48,9 +34,12 @@ func update_zoom(zoom_delta: Vector2) -> void:
 	zoom.y = clamp(zoom.y, zoom_min, zoom_max)
 
 func _process(delta):
-	# Reconsider using fps instead of delta
-	# var fps = Engine.get_frames_per_second()
-	
+	# Calculate the input direction
+	input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+
 	# Move the camera based on the input direction
 	if input_direction == Vector2.ZERO:
 		camera_motion = camera_motion.lerp(Vector2.ZERO, 0.2)
