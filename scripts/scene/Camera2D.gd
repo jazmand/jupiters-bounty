fea66be1f145b69e4e94b6dfac003b496a444bf2
jazmand <atothezmand@gmail.com>
2024-10-26 22:23:25 +0900

    Increase camera panning speed and update zoom to focus on mouse cursor (sorta)

--- scripts/scene/Camera2D.gd
+++ scripts/scene/Camera2D.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 var input_direction: Vector2 = Vector2.ZERO
 var camera_motion: Vector2 = Vector2.ZERO
-var camera_speed: float = 500.0
+var camera_speed: float = 1000.0
 
 var zoom_smoothing: float = 0.05
 var zoom_min: float = 0.2
@@ -14,7 +14,7 @@ func _input(event: InputEvent) -> void:
 		var motion_event: InputEventMouseButton = event
 		# mouse/trackpad scroll in: InputEventMouseButton and button_index = 4
 		# mouse/trackpad scroll out: InputEventMouseButton and button_index = 5
-		var zoom_delta = Vector2(1, 1)
+		var zoom_delta = Vector2(0.25, 0.25) 
 		if motion_event.button_index == 4:
 			update_zoom(zoom_delta)
 		elif motion_event.button_index == 5:
@@ -25,13 +25,19 @@ func _input(event: InputEvent) -> void:
 		var pan_event: InputEventPanGesture = event
 		var zoom_delta = Vector2(pan_event.delta.y, pan_event.delta.y)
 		update_zoom(zoom_delta)
-		
+
 func update_zoom(zoom_delta: Vector2) -> void:
+	var mouse_pos_before_zoom = get_global_mouse_position()
+
 	zoom += zoom_delta * zoom_smoothing
-	
 	# Clamp zoom value within the specified range
 	zoom.x = clamp(zoom.x, zoom_min, zoom_max)
 	zoom.y = clamp(zoom.y, zoom_min, zoom_max)
+
+	var mouse_pos_after_zoom = get_global_mouse_position()
+
+	# Adjust the camera position to keep mouse-centered zoom
+	position += (mouse_pos_before_zoom - mouse_pos_after_zoom)
 
 func _process(delta):
 	# Calculate the input direction
@@ -45,11 +51,4 @@ func _process(delta):
 		camera_motion = camera_motion.lerp(Vector2.ZERO, 0.2)
 	else:
 		camera_motion = input_direction * (delta * camera_speed)
-		
 	position += camera_motion
-	
-	# Clamp the camera's position to the desired boundaries
-	# TODO: Update boundaries
-	# Currently using Limit instead (see Inspector)
-#	position.x = clamp(position.x, 0, 0)
-#	position.y = clamp(position.y, 0, 0)
